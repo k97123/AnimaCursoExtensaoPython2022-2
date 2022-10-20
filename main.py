02f878f9d72a2cbed956c5ac1f4c76a07b804c7c
k97123 <45565528+k97123@users.noreply.github.com>
2022-10-20 00:53:51 +0000

    Aprendi a criar variáveis e printar na tela.

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ em bloco
 
 """
 
-print("Hello world!")
+print("Hello professor, hello world!")
+
+nome = 'Wanderson Santos Carvalho'
+
+idade = 24
+
+print('Esse código foi elaborado por', nome,', que sou eu. Eu tenho', idade, 'anos.')
