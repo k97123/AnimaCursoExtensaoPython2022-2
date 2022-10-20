7a76cc14dd24b4d6267bc2da38888b8d18d43848
k97123 <45565528+k97123@users.noreply.github.com>
2022-10-20 01:21:38 +0000

    Aprendi outras formas de printar na tela ( f"" ; '\n' ; {variável}; "{}, {}'.format(variavel1, variavel2)

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ em bloco
 
 """
 
-print("Hello professor, hello world!")
-
+print("Hello professor, hello world!\n")
+#print('')
 nome = 'Wanderson Santos Carvalho'
 
 idade = 24
 
-print('Esse código foi elaborado por', nome,', que sou eu. Eu tenho', idade, 'anos.')
+print('Esse código foi elaborado por {}, que sou eu. Eu tenho {} anos.\n'.format(nome,idade))
+
