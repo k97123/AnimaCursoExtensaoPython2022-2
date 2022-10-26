16b18e66ddfa10ade412906dccf7b4e883fc92c2
k97123 <45565528+k97123@users.noreply.github.com>
2022-10-26 23:15:25 +0000

    Aprendi a converter tipo de variavel e input().

---
+++ aula1-2022-10-19.py
@@ -0,0 +1,16 @@
+# Meu primeiro projeto Python!!!
+
+"""
+Comentario
+em bloco
+
+"""
+
+print("Hello professor, hello world!\n")
+#print('')
+nome = 'Wanderson Santos Carvalho'
+
+idade = 24
+
+print('Esse código foi elaborado por {}, que sou eu. Eu tenho {} anos.\n'.format(nome,idade))
+
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# Meu primeiro projeto Python!!!
+# comando input( ): quero permitir que o usuário digite algo...
+nome = input('Digite o seu nome: ')
+#pede a idade para o usuário "Qual a sua idade?"
+idade = int(input('Qual a sua idade: '))
 
-"""
-Comentario
-em bloco
 
-"""
+#comando de saída..exibir na tela
+print('Boa noite, o seu nome é: {}.\n'.format(nome))
+#exiba "Sua idade é..."
+print('{} sua idade é {} anos.\n'.format(nome,idade))
 
-print("Hello professor, hello world!\n")
-#print('')
-nome = 'Wanderson Santos Carvalho'
-
-idade = 24
-
-print('Esse código foi elaborado por {}, que sou eu. Eu tenho {} anos.\n'.format(nome,idade))
-
+#e se eu quisesse mostrar o dobro da idade?
+dobro = idade * 2
+triplo = idade * 3
+print("O dobro da idade é {} e o triplo é {}.\n".format(dobro,triplo))
